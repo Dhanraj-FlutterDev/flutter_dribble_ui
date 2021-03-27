34cc326ba6ccb58e4208876f45d4d8e53c20ee52
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2021-03-27 10:53:48 +0530

    Update congrats_page.dart

--- lib/screens/congrats_page.dart
+++ lib/screens/congrats_page.dart
@@ -1,1 +1,137 @@
-import 'package:flutter/cupertino.dart';import 'package:flutter/material.dart';import 'package:flutter_dribble_ui/screens/home_page.dart';class CongratsPage extends StatelessWidget {  @override  Widget build(BuildContext context) {    return Scaffold(      backgroundColor: Colors.indigo.shade700,      body: Container(        height: MediaQuery.of(context).size.height,        width: MediaQuery.of(context).size.width,        child: Column(          mainAxisAlignment: MainAxisAlignment.spaceEvenly,          children: [            Column(              children: [                Center(                  child: Container(                    height: 50,                    width: 50,                    margin: EdgeInsets.only(top: 30),                    decoration: BoxDecoration(                      shape: BoxShape.rectangle,                      color: Colors.white12,                      borderRadius: BorderRadius.circular(10),                    ),                    child: Padding(                      padding: EdgeInsets.all(8.0),                      child: CircleAvatar(                        backgroundColor: Colors.white,                        child: CircleAvatar(                          radius: 10,                          backgroundColor: Colors.indigo.shade700,                        )                      ),                    ),                  ),                ),                SizedBox(height: 5,),                Text('ofsp_ce',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,fontSize: 22),),              ],            ),            Stack(              fit: StackFit.loose,              children: [                Container(                  height: MediaQuery.of(context).size.height / 2,                  width: MediaQuery.of(context).size.width,                  decoration: BoxDecoration(                      image: DecorationImage(                        image: AssetImage('assets/image4.png'),                      ),                  ),                ),                Positioned(                  left: 60,                  right: 60,                  bottom: 0,                  child: Container(                    decoration: BoxDecoration(                      shape: BoxShape.rectangle,                      color: Colors.indigo.shade500,                      borderRadius: BorderRadius.circular(20),                    ),                    child: Column(                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,                      crossAxisAlignment: CrossAxisAlignment.center,                      children: [                        Text('Congrats! You Win',style: TextStyle(color: Colors.white,                            fontWeight: FontWeight.w700,fontSize: 25),),                        SizedBox(height: 5,),                        Text('You earn 300 Questpoin',style: TextStyle(color: Colors.white,                            fontWeight: FontWeight.w600,fontSize: 15),),                        SizedBox(height: 5,),                        Text('using flutter',style: TextStyle(color: Colors.white,                            fontWeight: FontWeight.w400,fontSize: 10),),                      ],                    ),                  ),                ),              ],            ),            SizedBox(height: 40,),            Container(              decoration: BoxDecoration(                gradient: LinearGradient(                  colors: [                    Colors.pink.shade500,                    Colors.pinkAccent.shade100,                  ]                ),                shape: BoxShape.rectangle,                borderRadius: BorderRadius.circular(10),              ),              height: 40,              width: MediaQuery.of(context).size.width / 1.2,              child: Center(                child: Text('Statistic',style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w600),),              ),            ),            InkWell(              onTap: (){                Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));              },              child: Container(                decoration: BoxDecoration(                  shape: BoxShape.rectangle,                  borderRadius: BorderRadius.circular(10),                ),                height: 40,                width: MediaQuery.of(context).size.width / 1.2,                child: Center(                  child: Text('Play Again',style: TextStyle(color: Colors.blueAccent,fontSize: 14,fontWeight: FontWeight.w600),),                ),              ),            ),          ],        ),      ),    );  }}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dribble_ui/screens/home_page.dart';
+
+class CongratsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo.shade700,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.only(top: 30),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.white12,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        child: CircleAvatar(
+                          radius: 10,
+                          backgroundColor: Colors.indigo.shade700,
+                        )
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5,),
+                Text('ofsp_ce',style: TextStyle(color: Colors.white,fontWeight: FontWeight.w900,fontSize: 22),),
+              ],
+            ),
+            Stack(
+              fit: StackFit.loose,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height / 2,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/image4.png'),
+                      ),
+                  ),
+                ),
+                Positioned(
+                  left: 60,
+                  right: 60,
+                  bottom: 0,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.indigo.shade500,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('Congrats! You Win',style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.w700,fontSize: 25),),
+                        SizedBox(height: 5,),
+                        Text('You earn 300 Questpoin',style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.w600,fontSize: 15),),
+                        SizedBox(height: 5,),
+                        Text('using flutter',style: TextStyle(color: Colors.white,
+                            fontWeight: FontWeight.w400,fontSize: 10),),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 40,),
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.pink.shade500,
+                    Colors.pinkAccent.shade100,
+                  ]
+                ),
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              height: 40,
+              width: MediaQuery.of(context).size.width / 1.2,
+              child: Center(
+                child: Text('Statistic',style: TextStyle(color: Colors.white,fontSize: 16,fontWeight: FontWeight.w600),),
+              ),
+            ),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                height: 40,
+                width: MediaQuery.of(context).size.width / 1.2,
+                child: Center(
+                  child: Text('Play Again',style: TextStyle(color: Colors.blueAccent,fontSize: 14,fontWeight: FontWeight.w600),),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
